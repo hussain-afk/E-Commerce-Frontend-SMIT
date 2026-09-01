1c9ce60ac44cc71e09357ddc25d9a588924ef85e
hussain kadir memon <hussainkadir245@gmail.com>
2026-09-01 17:35:43 +0500

    cors error fixed

--- src/api/auth.api.js
+++ src/api/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: 'https://e-commerce-backend-smit.vercel.app/',
+    baseURL: 'https://e-commerce-backend-smit.vercel.app',
     headers: {
         'Content-Type': 'application/json'
     },
